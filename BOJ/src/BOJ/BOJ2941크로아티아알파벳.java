c5ec4cba8a2d6dd0033d1cc033b51f7a7d1a5869
sumin9497 <sumin9497@naver.com>
2020-12-10 09:39:02 +0900

    크로아티아알파벳

---
+++ BOJ/src/BOJ/BOJ2941크로아티아알파벳.java
@@ -0,0 +1,25 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ2941크로아티아알파벳 {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.next();
+		String str[] = {"c=", "c-", "dz=", "d=", "lj", "nj", "s=", "z="};
+		
+		for (int i = 0; i < str.length; i++) {
+			s = s.replace(str[i], "a");
+		}
+		s = s.replace("-", "");
+		s = s.replace("=", "");
+		
+		System.out.println(s.length());
+		
+
+	}
+
+}
+
