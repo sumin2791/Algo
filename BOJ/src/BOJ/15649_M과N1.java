0b83c6749e0dbcbcfaf001418c7321cefc2c9423
sumin9497 <sumin9497@naver.com>
2022-01-03 01:37:38 +0900

    15649_M과N1

---
+++ BOJ/src/BOJ/15649_M과N1.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int arr[] = new int[N];
+        int sel[] = new int[M];
+        boolean v[] = new boolean[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1; // 1부터
+        }
+
+        permutation(arr, sel, v, 0, 0);
+
+    }
+
+    private static void permutation(int arr[], int sel[], boolean v[], int index, int k) {
+        if (k == sel.length) {
+            for (int i = 0; i < sel.length; i++) {
+                System.out.print(sel[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (v[i] != true) {
+                v[i] = true;
+                sel[k] = arr[i]; // 뽑기
+
+                permutation(arr, sel, v, i + 1, k + 1);
+                v[i] = false;
+
+            }
+
+        }
+
+    }
+}
