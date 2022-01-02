ed868aa99b5a6c8e19acf037c7ce6483bcebcfaa
sumin9497 <sumin9497@naver.com>
2022-01-03 01:37:15 +0900

    13300_방배정

---
+++ BOJ/src/BOJ/13300_ 방배정.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+
+        int student[][] = new int[6][2];
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int gender = sc.nextInt();
+            int grade = sc.nextInt();
+            student[grade - 1][gender]++;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 2; j++) {
+                int num = student[i][j];
+                ans += num / K;
+                if (num % K != 0)
+                    ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
