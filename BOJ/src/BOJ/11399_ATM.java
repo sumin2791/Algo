fa899965b9bcee4663e1601fe3334797d46df160
sumin9497 <sumin9497@naver.com>
2022-01-07 03:13:59 +0900

    11399_ATM

---
+++ BOJ/src/BOJ/11399_ATM.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int[] arr = new int[cnt];
+
+        for (int i = 0; i < cnt; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+        for (int i = 0; i < cnt; i++) {
+            answer += arr[i] * (cnt - i);
+        }
+
+        System.out.println(answer);
+
+    }
+}
