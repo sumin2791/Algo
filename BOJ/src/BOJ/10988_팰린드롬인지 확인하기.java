974d0adc7efe7a15642648a904f78d3b97dde4ef
sumin9497 <sumin9497@naver.com>
2022-01-07 03:04:05 +0900

    10988_팰린드롬인지 확인하기

---
+++ BOJ/src/BOJ/10988_팰린드롬인지 확인하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int answer = 1;
+        while (s.length() > 1) {
+            String first = s.substring(0, 1);
+            String last = s.substring(s.length() - 1);
+            if (first.equals(last)) {
+                s = s.substring(1, s.length() - 1);
+            } else {
+                answer = 0;
+                break;
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
