70d44542a4bcc2b53f7f8dc8f930a7cabdac5314
sumin9497 <sumin9497@naver.com>
2022-01-09 16:27:50 +0900

    1764_듣보잡

---
+++ BOJ/src/BOJ/1764_듣보잡.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        HashSet<String> set = new HashSet<>();
+        ArrayList<String> unknown = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(sc.next());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String s = sc.next();
+            if (set.contains(s)) {
+                unknown.add(s);
+            }
+        }
+
+        Collections.sort(unknown);
+        System.out.println(unknown.size());
+        for (int i = 0; i < unknown.size(); i++) {
+            System.out.println(unknown.get(i));
+        }
+
+    }
+}
