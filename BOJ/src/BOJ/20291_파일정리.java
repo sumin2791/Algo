451409f003f8e599193ffd9bd8c3af106554c76d
sumin9497 <sumin9497@naver.com>
2022-01-09 16:28:00 +0900

    20291_파일정리

---
+++ BOJ/src/BOJ/20291_파일정리.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        HashMap<String, Integer> map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            String file = sc.next();
+            String type = file.substring(file.indexOf(".") + 1);
+            if (map.containsKey(type)) {
+                int cnt = map.get(type);
+                map.put(type, cnt + 1);
+            } else {
+                map.put(type, 1);
+                list.add(type);
+            }
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            String name = list.get(i);
+            System.out.println("name " + map.get(name));
+        }
+
+    }
+}
