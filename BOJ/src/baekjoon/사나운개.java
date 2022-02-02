1034d968e07e46d30096f55ad5f739da96d37a48
sumin9497 <sumin9497@naver.com>
2022-02-02 18:24:20 +0900

    Fix 2991_사나운개

--- BOJ/src/baekjoon/사나운개.java
+++ BOJ/src/baekjoon/사나운개.java
@@ -1,5 +1,36 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class 사나운개 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A1 = sc.nextInt();
+		int B1 = sc.nextInt();
+		int A2 = sc.nextInt();
+		int B2 = sc.nextInt();
+		
+		int[] p = new int[3];
+		p[0] = sc.nextInt();
+		p[1] = sc.nextInt();
+		p[2] = sc.nextInt();
+		
+		for (int i = 0; i < p.length; i++) {
+			int ans = 0;
+			if(p[i]%(A1+B1)!=0) {
+				if(p[i]%(A1+B1)<=A1) {
+					ans++;
+				}
+			}
+			if(p[i]%(A2+B2)!=0) {
+				if(p[i]%(A2+B2)<=A2) {
+					ans++;
+				}
+			}
+			
+			System.out.println(ans);
+		}
+		
+	}
 }
